fdc2e16e12e3f13ccf129e478d37198d8cce25f5
Alberto <albertolabrada99@gmail.com>
2021-03-04 12:19:47 -0600

    avance inicial client

--- client/README.md
+++ client/README.md
@@ -1,4 +1,4 @@
-# MoviesNFun
+# Client
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.1.2.
 
--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'movies-n-fun'`, () => {
+  it(`should have as title 'mnf'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('movies-n-fun');
+    expect(app.title).toEqual('mnf');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('movies-n-fun app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('mnf app is running!');
   });
 });
