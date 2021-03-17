8613766e77436b1fe9086e3f274e76e25c4f2cb9
Alan Gonzalez <argaalan@hotmail.com>
2021-03-16 18:58:13 -0700

    Navbar, details on user and movie details

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import {MovieDetailComponent} from './pages/movie-detail/movie-detail.component';
 import {HomepageComponent} from './pages/homepage/homepage.component';
 import {ProfileDetailComponent} from './pages/profile-detail/profile-detail.component';
+import {LoginComponent} from './login/login.component';
 
 const routes: Routes = [{path: 'movie/:id', component: MovieDetailComponent},
                         {path: 'homepage', component: HomepageComponent},
+                        {path: 'sign-in', component: LoginComponent},
                         {path: 'profile', component: ProfileDetailComponent},
                         {path: '', redirectTo: 'homepage', pathMatch: 'full'}];
 
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,9 +7,10 @@ import { AppComponent } from './app.component';
 import { HomepageComponent } from './pages/homepage/homepage.component';
 import { MovieDetailComponent } from './pages/movie-detail/movie-detail.component';
 import { ProfileDetailComponent } from './pages/profile-detail/profile-detail.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [AppComponent, HomepageComponent, MovieDetailComponent, ProfileDetailComponent] ,
+  declarations: [AppComponent, HomepageComponent, MovieDetailComponent, ProfileDetailComponent, LoginComponent] ,
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
