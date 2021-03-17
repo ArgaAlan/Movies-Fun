9f3aa07642342cd2ab3e0a4a44ae233f31e222a9
Alberto <albertolabrada99@gmail.com>
2021-03-17 14:56:39 -0600

    Busqueda default y detalles de pelicula

--- client/src/app/pages/movie-detail/movie-detail.component.ts
+++ client/src/app/pages/movie-detail/movie-detail.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Movie } from 'src/app/models/movie.model';
+import { TraktService } from 'src/app/services/trakt.service';
 
 @Component({
   selector: 'app-movie-detail',
   templateUrl: './movie-detail.component.html',
-  styleUrls: ['./movie-detail.component.scss']
+  styleUrls: ['./movie-detail.component.scss'],
 })
 export class MovieDetailComponent implements OnInit {
-
-  constructor() { }
+  loading = true;
+  movie: Movie;
+  constructor(private route: ActivatedRoute, private trakt: TraktService) {}
 
   ngOnInit(): void {
+    const slug = this.route.snapshot.params['id'];
+    this.trakt.getMovieDetails(slug).subscribe((movie: Movie) => {
+      this.movie = movie;
+      console.log('this movie', this.movie);
+      this.loading = false;
+    });
   }
-
 }
