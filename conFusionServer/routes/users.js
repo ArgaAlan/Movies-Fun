6c6949267d3982faf4db5e3b0cbca357f76eee74
Alan Gonzalez <argaalan@hotmail.com>
2021-04-21 15:07:27 -0600

    Upates

--- conFusionServer/routes/users.js
+++ conFusionServer/routes/users.js
@@ -1,6 +1,6 @@
 var express = require('express');
 const bodyparser = require('body-parser');
-var Usuario = require('../models/usuario');
+var Usuario = require('../models/user');
 var passport = require('passport');
 var authenticate = require('../authenticate');
 
@@ -13,6 +13,7 @@ router.get('/', (req, res, next) => {
         res.stausCode = 200;
         res.setHeader('Content-Type', 'application/json');
         res.json(users);
+        console.log(users);
     }, (err) => next(err))
     .catch((err) => next(err));
 });
