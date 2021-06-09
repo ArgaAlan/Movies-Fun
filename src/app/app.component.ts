43121e304dac1d8032aeba79f00f14d080ffc87c
Alan Gonzalez <argaalan@hotmail.com>
2021-06-09 16:24:11 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { AuthService } from '@auth0/auth0-angular';
 import { Router } from '@angular/router';
+import {SwPush} from '@angular/service-worker';
 // import { AuthService } from './auth/auth.service';
 
 
@@ -13,8 +14,20 @@ import { Router } from '@angular/router';
 export class AppComponent implements OnInit, OnDestroy {
   // isAuthenticated = false;
   userSub: Subscription;
+  public readonly VAPID_PUBLIC_KEY = 'BAS8vsVukbdt3lOp-9UR2F4Mp_8B5a6QxJYT3qYYz1pBe9DWu3LJW8EM_PGantX6yDyRwJ53UbX_1J8JCGXxS-A';
 
-  constructor(public auth: AuthService, private router: Router) { }
+  constructor(public auth: AuthService, private router: Router, private swPush: SwPush) {
+
+  }
+
+  subscribeToNotifications(): any {
+    this.swPush.requestSubscription({
+      serverPublicKey: this.VAPID_PUBLIC_KEY
+    }).then(sub => {
+      const token = JSON.parse(JSON.stringify(sub));
+      console.log('****** ESPAÑOLETE DICIENDO OJO ******', token);
+    });
+  }
 
   // tslint:disable-next-line:typedef
   loginWithRedirect(){
